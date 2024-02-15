64797e514fc5742baa84addf2c1637be304a1d0a
duweri <danielleuweri01@gmail.com>
2024-02-15 15:24:29 +0000

    completed connection.js

---
+++ controllers/thoughtController.js
@@ -0,0 +1,35 @@
+const { Thought, User, Reaction } = require('../models');
+const { Types } = require('mongoose');
+
+module.exports = {
+  async getAllThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find();
+      res.json(thoughts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async getThoughtById(req, res) {
+    try {
+      const thought = await Thought.findOne({ _id: req.params.thoughtId });
+
+      if (!thought) {
+        return res.status(404).json({ message: 'Thought not found' });
+      } else {
+        res.json(thought);
+      }
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  },
+  // create a new post
+  async createThought(req, res) {
+    try {
+      const thought = await Thought.create(req.body);
+      res.status(201).json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+  }
+},
+
